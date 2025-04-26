066377dabca6193860021a03eaffa40a9825ff64
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-26 10:03:12 +0300

    models.py

--- weather/models.py
+++ weather/models.py
@@ -7,5 +7,8 @@ from database import Base
 class Weather(Base):
     __tablename__ = 'weather'
 
-
+    id = Column(Integer, primary_key=True, index=True)
+    city_id = Column(Integer, ForeignKey("city.id"), nullable=False)
+    city = relationship("City", back_populates="weather")
+    date_time = Column(Date, nullable=False)
     temperature = Column(Float, nullable=False)
