50bcb851fa4f74097b81b05e8e8b0fb1a7b6e71e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-26 10:02:44 +0300

    crud.py

--- city/crud.py
+++ city/crud.py
@@ -16,7 +16,10 @@ async def create_city(db: AsyncSession, city: schemas.CreateCity):
     query = insert(models.City).values(
         name=city.name,
         additional_info=city.additional_info
-
+    )
+    result = await db.execute(query)
+    await db.commit()
+    resp = {**city.dict(), "id": result.inserted_primary_key}
     return resp
 
 
